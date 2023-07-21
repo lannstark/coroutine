e57bfa5fedfc0f18b15c57d80bcdb75370214375
최태현 <choi-0202@kaist.ac.kr>
2023-07-21 16:05:44 +0900

    1강. 루틴과 코루틴

---
+++ src/main/kotlin/coroutine/Main.kt
@@ -0,0 +1,25 @@
+package coroutine
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
+
+fun main(): Unit = runBlocking {
+  printWithThread("START")
+  launch {
+    newRoutine()
+  }
+  yield()
+  printWithThread("END")
+}
+
+suspend fun newRoutine() {
+  val num1 = 1
+  val num2 = 2
+  yield()
+  printWithThread("${num1 + num2}")
+}
+
+fun printWithThread(str: Any) {
+  println("[${Thread.currentThread().name}] $str")
+}
